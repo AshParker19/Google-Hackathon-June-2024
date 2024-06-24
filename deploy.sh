c968eb0a43e7a5cbe88430f9a901750eb69be248
AshParker19 <ashparker4219@gmail.com>
2024-06-24 23:05:33 +0100

    fixed script

--- deploy.sh
+++ deploy.sh
@@ -7,26 +7,28 @@ then
     exit 1
 fi
 
-# get env variables
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs -d '\n')
-else
+# Check if .env file exists
+if [ ! -f .env ]; then
   echo "Error: .env file not found. Please create a .env file with the required environment variables."
   exit 1
 fi
 
-# variables
-SERVICE_NAME="sublime-lyceum-426907-r9"
-REGION="europe-west1"
+# Load environment variables, handling multiline variables separately
+export $(grep -v '^#' .env | grep -v 'GOOGLE_PRIVATE_KEY' | xargs)
+GOOGLE_PRIVATE_KEY=$(grep '^GOOGLE_PRIVATE_KEY' .env | cut -d '=' -f2-)
 
 # Encode the GOOGLE_PRIVATE_KEY to make it compatible with the deployment
-ENCODED_GOOGLE_PRIVATE_KEY=$(echo "$GOOGLE_PRIVATE_KEY" | base64 | tr -d '\n')
+ENCODED_GOOGLE_PRIVATE_KEY=$(echo -e "$GOOGLE_PRIVATE_KEY" | base64 | tr -d '\n')
 
 # Collect all other environment variables except GOOGLE_PRIVATE_KEY
 OTHER_ENV_VARS=$(grep -v '^#' .env | grep -v 'GOOGLE_PRIVATE_KEY' | xargs | sed 's/ /,/g')
 
 # Add the encoded GOOGLE_PRIVATE_KEY to the environment variables
 ENV_VARS="$OTHER_ENV_VARS,GOOGLE_PRIVATE_KEY=$ENCODED_GOOGLE_PRIVATE_KEY"
+
+# variables
+SERVICE_NAME="sublime-lyceum-426907-r9"
+REGION="europe-west1"
 
 # update requirements
 pip install -r requirements.txt > /dev/null 2>&1
