3e9345aa006c5f0e4bcf46b7fd59c7eb5a13e809
Anatolii <117525743+AshParker19@users.noreply.github.com>
2024-06-24 21:23:03 +0000

    handles password now

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
+import base64
 import json
 import os
 
@@ -15,21 +16,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SESSION_PSWD'
 app.config['SECRET_KEY'] = os.urandom(24)
 
-google_private_key = os.getenv('GOOGLE_PRIVATE_KEY')
-if google_private_key is None:
+encoded_private_key = os.getenv("GOOGLE_PRIVATE_KEY")
+if encoded_private_key is None:
     raise ValueError("GOOGLE_PRIVATE_KEY is not set")
+
+decoded_private_key = base64.b64decode(encoded_private_key).decode("utf-8")
 
 creds_dict = {
     "type": os.getenv('GOOGLE_TYPE', ''),
     "project_id": os.getenv('GOOGLE_PROJECT_ID', ''),
     "private_key_id": os.getenv('GOOGLE_PRIVATE_KEY_ID', ''),
-    "private_key": google_private_key.replace('\\n', '\n'),
+    "private_key": decoded_private_key.replace('\\n', '\n'),
     "client_email": os.getenv('GOOGLE_CLIENT_EMAIL', ''),
     "client_id": os.getenv('GOOGLE_CLIENT_ID', ''),
     "auth_uri": os.getenv('GOOGLE_AUTH_URI', ''),
     "token_uri": os.getenv('GOOGLE_TOKEN_URI', ''),
     "auth_provider_x509_cert_url": os.getenv('GOOGLE_AUTH_PROVIDER_X509_CERT_URL', ''),
     "client_x509_cert_url": os.getenv('GOOGLE_CLIENT_X509_CERT_URL', '')
+}
 }
 
 # create a db instance
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Check if gcloud is installed and available in the PATH
+if ! command -v gcloud &> /dev/null
+then
+    echo "Error: gcloud command not found. Please install Google Cloud SDK and add it to your PATH."
+    exit 1
+fi
+
 # get env variables
 if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+  export $(grep -v '^#' .env | xargs -d '\n')
 else
   echo "Error: .env file not found. Please create a .env file with the required environment variables."
   exit 1
@@ -11,7 +18,15 @@ fi
 # variables
 SERVICE_NAME="sublime-lyceum-426907-r9"
 REGION="europe-west1"
-ENV_VARS=$(grep -v '^#' .env | xargs | sed 's/ /,/g')
+
+# Encode the GOOGLE_PRIVATE_KEY to make it compatible with the deployment
+ENCODED_GOOGLE_PRIVATE_KEY=$(echo "$GOOGLE_PRIVATE_KEY" | base64 | tr -d '\n')
+
+# Collect all other environment variables except GOOGLE_PRIVATE_KEY
+OTHER_ENV_VARS=$(grep -v '^#' .env | grep -v 'GOOGLE_PRIVATE_KEY' | xargs | sed 's/ /,/g')
+
+# Add the encoded GOOGLE_PRIVATE_KEY to the environment variables
+ENV_VARS="$OTHER_ENV_VARS,GOOGLE_PRIVATE_KEY=$ENCODED_GOOGLE_PRIVATE_KEY"
 
 # update requirements
 pip install -r requirements.txt > /dev/null 2>&1
